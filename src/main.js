20343e8372e51295226d5646a6a93bd6f1702a06
pianduan <pianduan95@gmail.com>
2022-11-08 14:59:19 +0800

    feat: 新增 pinia

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import { setupRouter } from "@/router";
+import { setupStore } from "@/store";
 import App from "./App.vue";
 
 async function setupApp() {
   const app = createApp(App);
+  setupStore(app);
   await setupRouter(app);
   app.mount("#app");
 }
